7d311b610078dbde33e9023f319d00e0ae3b18b2
dbwls <youjin9874@naver.com>
2021-07-23 13:24:31 +0900

    quiz01 생성

---
+++ src/day01/Quiz01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Quiz01 {
+	public static void main(String[] args) {
+		System.out.println("=====================================");
+		System.out.println("이름     숫자      전화번호         회비 ");
+		System.out.println("=====================================");
+		System.out.println("홍길동    \"15\"    3672-1234    \\20000 ");
+		System.out.println("고길동    \"15\"    2238-1234    \\30000 ");
+		System.out.println("김말이    \"15\"    1234-1234    \\50000 ");
+		System.out.println("-------------------------------------");
+		System.out.println("총합계                         \\100000");
+		System.out.println("-------------------------------------");
+	}
+	
+
+}
