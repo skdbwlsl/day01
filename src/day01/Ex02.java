b9c43e77c1f785d9e4b3a14a3dd0c625f4eeb167
dbwls <youjin9874@naver.com>
2021-07-23 12:49:05 +0900

    문자열 관련 기능

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -1,13 +1,19 @@
 package day01;
 
 public class Ex02 {
-	public static void main String[] args){
+	public static void main (String[] args){
 		/*
 		 \n : 엔터와 비슷하다
 		 \n : 탭크개ㅣ만큼 오른쪽으로 이동(윈도우에서는 대략 8칸 정도)
 		 \" : "(큰따옴표) 표시 (일반 문자로 사용하고자 할때)
 		 \\ : \(역슬래쉬) 표현할 떄 사용
 		 */
+		
+		
+		// "" : 쌍따옴표로 묶인 값은 문자열로 인식한다
+		System.out.println("h\nello java");
+		System.out.println("1\t1234567\t12345678\t12");
+		System.out.println("test \"123\" \\456");
 	}
 
 }
